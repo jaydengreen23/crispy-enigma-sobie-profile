b79f7fac4ca52a15b720b290ee4990647619ad37
Green <jgreen28@una.edu>
2025-01-21 16:48:38 -0600

    words

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-//endpoint, middleware(s)
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function (req, res) {
-  res.send('Hello Express')
+  res.sendFile('index.html')
 })
+
+app.get('/nodemon', function (req, res) {
+  res.sendFile('look ma, something LOlLLO')
+})
+
+app.get('/ejs', function (req, res) {
+  res.render('words', 
+    {pageTitle: 'my cool ejs page'}
+  );
+})
+
+//endpoint, middleware(s)
+app.get('/helloRender', function (req, res) {
+  res.send('Hello Express from Real World<br><a href="/">back to home</a>')
+})
+
 
 app.listen(port, ()=> console.log(`server is running on .. ${port}`));
