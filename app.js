94843b4e16bbdcbca4ad7c24505ac16e68e745a8
Green <jgreen28@una.edu>
2025-01-23 16:34:08 -0600

    Update2

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
   res.sendFile('index.html')
 })
 
+app.get('/saveMyName', (req,res)=>{
+  console.log('GET the end');
+  console.log(req.query);
+
+  res.redirect('/ejs');
+})
+
+app.post('/saveMyName', (req,res)=>{
+  console.log('POST the end');
+  console.log(req.body);
+
+ // res.redirect('/ejs');
+ res.render('words',
+  {pageTitle: req.body.myName}
+ );
+})
+
 app.get('/nodemon', function (req, res) {
-  res.sendFile('look ma, something LOlLLO')
+  res.send('look ma, something LOlLLO')
 })
 
 app.get('/ejs', function (req, res) {
   res.render('words', 
-    {pageTitle: 'my cool ejs page'}
+    {pageTitle: req.body.myName}
   );
 })
 
