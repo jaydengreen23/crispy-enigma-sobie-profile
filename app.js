9a746b1448200a790f8a14d958f316cb480bb974
Green <jgreen28@una.edu>
2025-01-28 16:41:47 -0600

    mongodp

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =process.env.MONGO_URI;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'))
+
+//begin all my middle wares
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile('index.html')
@@ -15,8 +48,14 @@ app.get('/saveMyName', (req,res)=>{
   console.log('GET the end');
   console.log(req.query);
 
-  res.redirect('/ejs');
+  let reqName = req.query.myName;
+
+  res.render('words',
+  {pageTitle: reqName}
+ );
 })
+
+  //res.redirect('/ejs');
 
 app.post('/saveMyName', (req,res)=>{
   console.log('POST the end');
